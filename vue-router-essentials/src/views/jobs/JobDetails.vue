1bdf80d6d6a223c04b275f4a671841edfc45aa9c
Daniel <danielselga@gmail.com>
2021-07-20 23:57:05 -0300

    fetching data

--- vue-router-essentials/src/views/jobs/JobDetails.vue
+++ vue-router-essentials/src/views/jobs/JobDetails.vue
@@ -1,13 +1,31 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The Job Details id is {{id}} </p>
-  <!-- dentro do template não precisamos chamar o $route.params usando o this já na func data precisamos usar. -->
+  <div v-if="job">
+    <h1>{{job.title}}</h1>
+    <p>The Job id is {{id}} </p>
+    <!-- dentro do template não precisamos chamar o $route.params usando o this já na func data precisamos usar. -->
+    <p>{{job.details}}</p>
+  </div>
+  <div v-else>
+    <p>Loading job Details...</p>
+  </div>
 </template>
 
 <script>
 export default {
 props: ['id'],
 
+data(){
+  return {
+    job: null
+  }
+},
+mounted(){
+        //um lugar popular para dar fetch em dados.
+         fetch('http://localhost:3000/jobs/' + this.id)
+         .then((res) => res.json())
+         .then(data => this.job = data)
+         .catch(err => console.log(err.message))
+    }
 // Aqui estamos usando o this para chamar o $route.params
 //     data(){
 //         return {
--- vue-router-essentials/src/views/jobs/Jobs.vue
+++ vue-router-essentials/src/views/jobs/Jobs.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
       <h1>jobs</h1>
-      <div v-for="job in jobs" :key="job.id">
-          <router-link :to="{name: 'JobDetails', params: {id: job.id}}">
-              <!-- aqui estamos setando para a pagina jobdetails e passando como parametro o id setado no objeto jobs -->
-              <!-- esse parametro vai ser passado lá no componente jobDetails que vai plotar na tela até então -->
-              <h2>{{job.title}}</h2>
-          </router-link>
-      </div>
+      
+        <div v-if="jobs.length">
+            <div v-for="job in jobs" :key="job.id">
+            <router-link :to="{name: 'JobDetails', params: {id: job.id}}">
+                <!-- aqui estamos setando para a pagina jobdetails e passando como parametro o id setado no objeto jobs -->
+                <!-- esse parametro vai ser passado lá no componente jobDetails que vai plotar na tela até então -->
+                <h2>{{job.title}}</h2>
+            </router-link>
+        </div>
+        </div>
+        <div v-else>
+            <p>Loading jobs...</p>
+        </div>
   </div>
 </template>
 
@@ -15,12 +21,15 @@
 export default {
     data(){
         return {
-            jobs: [
-                {title: 'Ux Designer', id: 1, details: 'lorem'},
-                {title: 'Web Developer', id: 2, details: 'lorem'},
-                {title: 'Vue Developer', id: 3, details: 'lorem'},
-            ]
+            jobs: []
         }
+    },
+    mounted(){
+        //um lugar popular para dar fetch em dados.
+         fetch('http://localhost:3000/jobs')
+         .then((res) => res.json())
+         .then(data => this.jobs = data)
+         .catch(err => console.log(err.message))
     }
 }
 </script>
